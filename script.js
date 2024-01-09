723ab6a89c6639fb636e93dda2ce28f4de05b37d
Suyash Kadam <122480150+SuyashGaneshKadam@users.noreply.github.com>
2024-01-10 02:34:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-function makeid(l) {
+function makeid(length) {
   // write your code here
+	let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  for ( let i = 0; i < length; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
 }
 
 // Do not change the code below.
